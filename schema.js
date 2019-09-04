e64f8ec902c73cc3bc84ead5f8033b2c10e42d32
Timothy Ho <tho@cra.com>
2019-09-04 17:58:53 -0400

    schema.js now uses data from 'json-server' instead of hardcoded ones

--- schema.js
+++ schema.js
@@ -1,3 +1,5 @@
+const axios = require('axios');
+
 const {
   GraphQLObjectType,
   GraphQLString,
@@ -7,14 +9,14 @@ const {
   GraphQLNonNull
 } = require('graphql');
 
-// hardcode data
+/* hardcode data
 const customers = [
   { id: '1', name: 'Althilus', email: 'a@blah.com', age: 25 },
   { id: '2', name: 'Lapse', email: 'l@blah.com', age: 26 },
   { id: '3', name: 'Mayple', email: 'm@blah.com', age: 24 },
   { id: '4', name: 'Dawn', email: 'd@blah.com', age: 25 }
 ];
-
+*/
 
 // Customer Type
 const CustomerType = new GraphQLObjectType({
@@ -37,17 +39,20 @@ const RootQuery = new GraphQLObjectType({
         id: { type: GraphQLString }
       },
       resolve(parentValue, args) {
-        for (let i = 0; i < customers.length; i++) {
+        /*for (let i = 0; i < customers.length; i++) {
           if (customers[i].id == args.id) {
             return customers[i];
           }
-        }
+        }*/
+        return axios.get(`http://localhost:3000/customers/${args.id}`)
+          .then(response => response.data);
       }
     },
     customers: {
       type: new GraphQLList(CustomerType),
       resolve(parentValue, args) {
-        return customers;
+        //return customers;
+        return axios.get('http://localhost:3000/customers').then(res => res.data);
       }
     }
   }
